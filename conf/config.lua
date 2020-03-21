db013f7cfca5788d1c05a6fa77db49cffd58914c
Raymond Marcil <marcilr@gmail.com>
2020-03-21 15:33:41 -0800

    config.lua   $HOME/.imapfilter/config.lua   imapfilter configruation file

---
+++ conf/config.lua
@@ -0,0 +1,20 @@
+--[[
+config.lua
+Created Sat 21 Mar 2020 03:20:48 PM AKDT
+Copyright (C) 2020 by Raymond E. Marcil <marcilr@gmail.com>
+--]]
+
+--[[
+Example config.lua configuration:
+
+myaccount = IMAP {
+    server = 'imap.mail.server',
+    username = 'me',
+    password = 'secret',
+    ssl = 'ssl3'
+}
+
+--1.3 – Some Lexical Conventions
+  https://www.lua.org/pil/1.3.html
+
+--]]
